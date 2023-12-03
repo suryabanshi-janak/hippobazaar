edb3c2bb322b151db1e4e688067a8f0180b93b31
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-12-03 20:56:28 +0545

    auth buttons

---
+++ src/components/Cart.tsx
@@ -0,0 +1,9 @@
+import { ShoppingCart } from 'lucide-react';
+
+export default function Cart() {
+  return (
+    <div>
+      <ShoppingCart aria-hidden='true' className='h-6 w-6  text-gray-400 ' />
+    </div>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,12 @@ import Link from 'next/link';
 import MaxWidthWrapper from './MaxWidthWrapper';
 import { Icons } from './Icons';
 import NavItems from './NavItems';
+import { buttonVariants } from './ui/button';
+import Cart from './Cart';
 
 export default function Navbar() {
+  const user = null;
+
   return (
     <div className='sticky top-0 inset-x-0'>
       <header className='relative bg-white'>
@@ -20,6 +24,40 @@ export default function Navbar() {
 
               <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
                 <NavItems />
+              </div>
+
+              <div className='flex items-center ml-auto'>
+                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                  {user ? null : (
+                    <Link
+                      href='/sign-in'
+                      className={buttonVariants({ variant: 'ghost' })}
+                    >
+                      Sign in
+                    </Link>
+                  )}
+
+                  {user ? null : (
+                    <span className='h-6 w-px bg-gray-200' aria-hidden='true' />
+                  )}
+
+                  {user ? (
+                    <div></div>
+                  ) : (
+                    <Link
+                      href='/sign-up'
+                      className={buttonVariants({ variant: 'ghost' })}
+                    >
+                      Create account
+                    </Link>
+                  )}
+
+                  <span className='h-6 w-px bg-gray-200' aria-hidden='true' />
+
+                  <div className='ml-4 flow-root lg:ml-6'>
+                    <Cart />
+                  </div>
+                </div>
               </div>
             </div>
           </MaxWidthWrapper>
