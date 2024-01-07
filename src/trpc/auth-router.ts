98ff7b5f8c49f8fe40cdd90a3b417a98b0d6ecd9
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-01-07 18:30:17 +0545

    signin api

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -1,8 +1,8 @@
-import { AuthCredentialsValidator } from "../lib/validators/auth-credentials-validator";
-import { publicProcedure, router } from "./trpc";
-import { getPayloadClient } from "../get-payload";
-import { TRPCError } from "@trpc/server";
-import { z } from "zod";
+import { AuthCredentialsValidator } from '../lib/validators/auth-credentials-validator';
+import { publicProcedure, router } from './trpc';
+import { getPayloadClient } from '../get-payload';
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
 
 export const authRouter = router({
   createPayloadUser: publicProcedure
@@ -12,7 +12,7 @@ export const authRouter = router({
       const payload = await getPayloadClient();
 
       const { docs: users } = await payload.find({
-        collection: "users",
+        collection: 'users',
         where: {
           email: {
             equals: email,
@@ -20,14 +20,14 @@ export const authRouter = router({
         },
       });
 
-      if (users.length !== 0) throw new TRPCError({ code: "CONFLICT" });
+      if (users.length !== 0) throw new TRPCError({ code: 'CONFLICT' });
 
       await payload.create({
-        collection: "users",
+        collection: 'users',
         data: {
           email,
           password,
-          role: "user",
+          role: 'user',
         },
       });
 
@@ -42,12 +42,36 @@ export const authRouter = router({
       const payload = await getPayloadClient();
 
       const isVerified = await payload.verifyEmail({
-        collection: "users",
+        collection: 'users',
         token,
       });
 
-      if (!isVerified) throw new TRPCError({ code: "UNAUTHORIZED" });
+      if (!isVerified) throw new TRPCError({ code: 'UNAUTHORIZED' });
 
       return { success: true };
+    }),
+
+  signIn: publicProcedure
+    .input(AuthCredentialsValidator)
+    .mutation(async ({ input, ctx }) => {
+      const { email, password } = input;
+      const { res } = ctx;
+
+      const payload = await getPayloadClient();
+
+      try {
+        await payload.login({
+          collection: 'users',
+          data: {
+            email,
+            password,
+          },
+          res,
+        });
+
+        return { success: true };
+      } catch (error) {
+        throw new TRPCError({ code: 'UNAUTHORIZED' });
+      }
     }),
 });
