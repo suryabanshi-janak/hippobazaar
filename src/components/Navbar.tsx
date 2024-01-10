c3ff55efe210a6432072d11fdb55171e0dfc9d51
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-01-10 19:31:44 +0545

    getting user in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import Link from 'next/link';
+import { cookies } from 'next/headers';
 import MaxWidthWrapper from './MaxWidthWrapper';
 import { Icons } from './Icons';
 import NavItems from './NavItems';
 import { buttonVariants } from './ui/button';
 import Cart from './Cart';
+import { getServerSideUser } from '@/lib/payload-utils';
 
-export default function Navbar() {
-  const user = null;
+export default async function Navbar() {
+  const nextCookies = cookies();
+  const { user } = await getServerSideUser(nextCookies);
 
   return (
     <div className='sticky top-0 inset-x-0'>
