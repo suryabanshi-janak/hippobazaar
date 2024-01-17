f40d50c3e6bc9a455ed7df462e13c6c2b79bb6dd
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-01-17 11:15:48 +0545

    data modelling

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,10 +5,14 @@ import { mongooseAdapter } from '@payloadcms/db-mongodb';
 import { slateEditor } from '@payloadcms/richtext-slate';
 import path from 'path';
 import { Users } from './collections/Users';
+import { Products } from './collections/Products/Product';
+import { ProductFiles } from './collections/ProductFile';
+import { Media } from './collections/Media';
+import { Orders } from './collections/Orders';
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  collections: [Users],
+  collections: [Users, Products, ProductFiles, Media, Orders],
   routes: {
     admin: '/sell',
   },
