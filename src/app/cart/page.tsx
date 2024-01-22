8714306e616b92bbe613effab1a691653dba3c83
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-01-22 22:14:22 +0545

    list cart products

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
-import { Loader2 } from 'lucide-react';
+import { Check, Loader2, X } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import { useCart } from '@/hooks/use-cart';
-import { cn, formatPrice } from '@/lib/utils';
+import { cn, formatPrice, productLabel } from '@/lib/utils';
 
 export default function Cart() {
   const { items, removeItem } = useCart();
@@ -58,6 +59,82 @@ export default function Cart() {
                 </p>
               </div>
             ) : null}
+
+            <ul
+              className={cn({
+                'divide-y divide-gray-200 border-b border-t border-gray-200':
+                  isMounted && items.length > 0,
+              })}
+            >
+              {isMounted &&
+                items.map(({ product }) => {
+                  const label = productLabel(product.category);
+
+                  const { image } = product.images[0];
+
+                  return (
+                    <li key={product.id} className='flex py-6 sm:py-10'>
+                      <div className='flex-shrink-0'>
+                        <div className='relative h-24 w-24'>
+                          {typeof image !== 'string' && image.url ? (
+                            <Image
+                              fill
+                              src={image.url}
+                              alt='product image'
+                              className='h-full w-full rounded-md object-cover object-center sm:h-48 sm:w-48'
+                            />
+                          ) : null}
+                        </div>
+                      </div>
+
+                      <div className='ml-4 flex flex-1 flex-col justify-between sm:ml-6'>
+                        <div className='relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0'>
+                          <div>
+                            <div className='flex justify-between'>
+                              <h3 className='text-sm'>
+                                <Link
+                                  href={`/product/${product.id}`}
+                                  className='font-medium text-gray-700 hover:text-gray-800'
+                                >
+                                  {product.name}
+                                </Link>
+                              </h3>
+                            </div>
+
+                            <div className='mt-1 flex text-sm'>
+                              <p className='text-muted-foreground'>
+                                Category: {label}
+                              </p>
+                            </div>
+
+                            <p className='mt-1 text-sm font-medium text-gray-900'>
+                              {formatPrice(product.price)}
+                            </p>
+                          </div>
+
+                          <div className='mt-4 sm:mt-0 sm:pr-9 w-20'>
+                            <div className='absolute right-0 top-0'>
+                              <Button
+                                aria-label='remove product'
+                                onClick={() => removeItem(product.id)}
+                                variant='ghost'
+                              >
+                                <X className='h-5 w-5' aria-hidden='true' />
+                              </Button>
+                            </div>
+                          </div>
+                        </div>
+
+                        <p className='mt-4 flex space-x-2 text-sm text-gray-700'>
+                          <Check className='h-5 w-5 flex-shrink-0 text-green-500' />
+
+                          <span>Eligible for instant delivery</span>
+                        </p>
+                      </div>
+                    </li>
+                  );
+                })}
+            </ul>
           </div>
 
           <section className='mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8'>
