281ec30c1bdf689848e2a46b104f73d9a1ae8912
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-01-23 10:24:14 +0545

    payment router

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,9 +3,11 @@ import { authRouter } from './auth-router';
 import { publicProcedure, router } from './trpc';
 import { getPayloadClient } from '../get-payload';
 import { QueryValidator } from '../lib/validators/query-validators';
+import { paymentRouter } from './payment-router';
 
 export const appRouter = router({
   auth: authRouter,
+  payment: paymentRouter,
 
   getInfiniteProducts: publicProcedure
     .input(
