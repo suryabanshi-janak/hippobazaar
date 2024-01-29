54546fd2d70f84fd5b0cb4c051009e0d1d65d0e6
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-01-29 22:13:05 +0545

    webhook

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import express from 'express';
-import { getPayloadClient } from './get-payload';
-import { nextApp, nextHandler } from './next-utils';
-import * as trpcExpress from '@trpc/server/adapters/express';
-import { appRouter } from './trpc';
-import { inferAsyncReturnType } from '@trpc/server';
+import express from "express";
+import { getPayloadClient } from "./get-payload";
+import { nextApp, nextHandler } from "./next-utils";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { appRouter } from "./trpc";
+import { inferAsyncReturnType } from "@trpc/server";
+import bodyParser from "body-parser";
+import { IncomingMessage } from "http";
+import { stripeWebhookHandler } from "./webhooks";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -18,7 +21,19 @@ const createContext = ({
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext>;
 
+export type WebhookRequest = IncomingMessage & {
+  rawBody: Buffer;
+};
+
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer;
+    },
+  });
+
+  app.post("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler);
+
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
@@ -28,8 +43,21 @@ const start = async () => {
     },
   });
 
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info("Next.js is building for production");
+
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, "../"));
+
+      process.exit();
+    });
+
+    return;
+  }
+
   app.use(
-    '/api/trpc',
+    "/api/trpc",
     trpcExpress.createExpressMiddleware({
       router: appRouter,
       createContext,
@@ -39,7 +67,7 @@ const start = async () => {
   app.use((req, res) => nextHandler(req, res));
 
   nextApp.prepare().then(() => {
-    payload.logger.info('Next.js started');
+    payload.logger.info("Next.js started");
 
     app.listen(PORT, async () => {
       payload.logger.info(
