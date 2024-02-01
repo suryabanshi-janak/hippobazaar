3c312de3c683e914ecc32eef863902ba04f17221
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-02-01 09:03:18 +0545

    middleware and cart routes

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,4 +1,14 @@
-import { CollectionConfig } from 'payload/types';
+import { CollectionConfig, Access } from 'payload/types';
+
+const adminsAndUser: Access = ({ req: { user } }) => {
+  if (user.role === 'admin') return true;
+
+  return {
+    id: {
+      equals: user.id,
+    },
+  };
+};
 
 export const Users: CollectionConfig = {
   slug: 'users',
@@ -10,17 +20,41 @@ export const Users: CollectionConfig = {
     },
   },
   access: {
-    read: () => true,
+    read: adminsAndUser,
     create: () => true,
+    update: ({ req }) => req.user.role === 'admin',
+    delete: ({ req }) => req.user.role === 'admin',
   },
   admin: {
     hidden: ({ user }) => user.role !== 'admin',
+    defaultColumns: ['id'],
   },
   fields: [
+    {
+      name: 'products',
+      label: 'Products',
+      admin: {
+        condition: () => false,
+      },
+      type: 'relationship',
+      relationTo: 'products',
+      hasMany: true,
+    },
+    {
+      name: 'product_files',
+      label: 'Product files',
+      admin: {
+        condition: () => false,
+      },
+      type: 'relationship',
+      relationTo: 'product_files',
+      hasMany: true,
+    },
     {
       name: 'role',
       defaultValue: 'user',
       required: true,
+
       type: 'select',
       options: [
         { label: 'Admin', value: 'admin' },
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import express from "express";
-import { getPayloadClient } from "./get-payload";
-import { nextApp, nextHandler } from "./next-utils";
 import * as trpcExpress from "@trpc/server/adapters/express";
-import { appRouter } from "./trpc";
 import { inferAsyncReturnType } from "@trpc/server";
 import bodyParser from "body-parser";
-import { IncomingMessage } from "http";
-import { stripeWebhookHandler } from "./webhooks";
+import { PayloadRequest } from "payload/types";
 import nextBuild from "next/dist/build";
 import path from "path";
+import { IncomingMessage } from "http";
+import { parse } from "url";
+
+import { getPayloadClient } from "./get-payload";
+import { nextApp, nextHandler } from "./next-utils";
+import { appRouter } from "./trpc";
+import { stripeWebhookHandler } from "./webhooks";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -58,6 +61,22 @@ const start = async () => {
     return;
   }
 
+  const cartRouter = express.Router();
+
+  cartRouter.use(payload.authenticate);
+
+  cartRouter.get("/", (req, res) => {
+    const request = req as PayloadRequest;
+
+    if (!request.user) return res.redirect("/sign-in?origin=cart");
+
+    const parsedUrl = parse(req.url, true);
+    const { query } = parsedUrl;
+
+    return nextApp.render(req, res, "/cart", query);
+  });
+
+  app.use("/cart", cartRouter);
   app.use(
     "/api/trpc",
     trpcExpress.createExpressMiddleware({
