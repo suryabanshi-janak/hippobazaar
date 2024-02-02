baf6da17e8c9ab49e46d0d9a8d7353531bd1e396
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-02-02 22:45:25 +0545

    primary action email

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,7 +1,8 @@
-import { CollectionConfig, Access } from 'payload/types';
+import { CollectionConfig, Access } from "payload/types";
+import { PrimaryActionEmailHtml } from "../components/emails/PrimaryActionEmail";
 
 const adminsAndUser: Access = ({ req: { user } }) => {
-  if (user.role === 'admin') return true;
+  if (user.role === "admin") return true;
 
   return {
     id: {
@@ -11,54 +12,58 @@ const adminsAndUser: Access = ({ req: { user } }) => {
 };
 
 export const Users: CollectionConfig = {
-  slug: 'users',
+  slug: "users",
   auth: {
     verify: {
       generateEmailHTML: ({ token }) => {
-        return `<a href='${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}'>Verify account</a>`;
+        return PrimaryActionEmailHtml({
+          actionLabel: "verify your account",
+          buttonText: "Verify Account",
+          href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`,
+        });
       },
     },
   },
   access: {
     read: adminsAndUser,
     create: () => true,
-    update: ({ req }) => req.user.role === 'admin',
-    delete: ({ req }) => req.user.role === 'admin',
+    update: ({ req }) => req.user.role === "admin",
+    delete: ({ req }) => req.user.role === "admin",
   },
   admin: {
-    hidden: ({ user }) => user.role !== 'admin',
-    defaultColumns: ['id'],
+    hidden: ({ user }) => user.role !== "admin",
+    defaultColumns: ["id"],
   },
   fields: [
     {
-      name: 'products',
-      label: 'Products',
+      name: "products",
+      label: "Products",
       admin: {
         condition: () => false,
       },
-      type: 'relationship',
-      relationTo: 'products',
+      type: "relationship",
+      relationTo: "products",
       hasMany: true,
     },
     {
-      name: 'product_files',
-      label: 'Product files',
+      name: "product_files",
+      label: "Product files",
       admin: {
         condition: () => false,
       },
-      type: 'relationship',
-      relationTo: 'product_files',
+      type: "relationship",
+      relationTo: "product_files",
       hasMany: true,
     },
     {
-      name: 'role',
-      defaultValue: 'user',
+      name: "role",
+      defaultValue: "user",
       required: true,
 
-      type: 'select',
+      type: "select",
       options: [
-        { label: 'Admin', value: 'admin' },
-        { label: 'User', value: 'user' },
+        { label: "Admin", value: "admin" },
+        { label: "User", value: "user" },
       ],
     },
   ],
